91bfe43f4eae420a8707cd6b945eadccacfc48df
juancgn <150616950+juancgn@users.noreply.github.com>
2024-07-15 08:19:22 +0200

    improves formatting, documentation and small cosmetics

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Twitterbot
 
-Twitterbot is a Python-based bot for Twitter that reads tweets (called `quotes`) from a local SQLite database and automatically posts them on Twitter. I wrote it to post funny quotes from my favorite character from a CBS show automatically. The bot uses the Tweepy library for interacting with the Twitter API v2 and is designed to post at configurable times. Twitterbot only uses the free-of-charge tools of the Twitter API.
+Twitterbot is a Python-based bot for Twitter that reads tweets (called `quotes`) from a local SQLite database and posts them automatically on Twitter. I wrote it to post funny quotes from my favorite character from a CBS show. The bot uses the Tweepy library for interacting with the Twitter API v2 and is designed to post at configurable times. Twitterbot only uses the free-of-charge tools of the Twitter API.
 
-Twitterbot is designed to run permanently and therefore you should run it on a server, a Raspberry Pi oder another systems which is up permanently.
+Twitterbot is designed to run permanently and therefore you should run it on a server, a Raspberry Pi oder similar.
 
 ## Features
 
 - **Database Connection:** The bot establishes a connection to a local SQLite database that manages quotes, queueing, and post logging.
-- **Tweet Posting:** Quotes are posted as tweets using the Twitter API with Tweepy.
+- **Tweet Posting:** Quotes are posted as tweets using the Twitter API v2 with Tweepy.
 - **Scheduling:** Multiple schedule options are provided which take care of the posting timing
 - **Logging:** Events and errors are logged to assist with performance monitoring and troubleshooting.
-- **Queueing:** Used quotes will be queued randomly in the second half of the queue, so the quote selection is random enough but a quote is definitely not posted again too soon.
+- **Queueing:** Used quotes will be queued randomly in the second half of the queue, so the quote selection is random enough but an used quote is not posted again too soon.
 
 ## Requirements
 
@@ -25,23 +25,23 @@ Twitterbot is designed to run permanently and therefore you should run it on a s
    git clone https://github.com/juancgn/twitterbot.git
    cd twitterbot
    ```
-2. Install tweepy with 
+2. Install requirements with 
     ```
     pip install -r requirements.txt
     ```
 ## Usage
 
-1. Write your quotes in a new file named `rawdata.txt`, one per line. Ensure they do not exceed the allowd Tweet length. Copy this file to your server using, for example, ssh:
+1. Write your quotes in a new file named `rawdata.txt`, one per line. Ensure they do not exceed the allowed Tweet length. Copy this file to your server using, for example, ssh:
 
     ```
     scp [-i ssh_private_key] /path/on/your/computer/rawdata.txt username@SERVER_IP:/path/to/directory/of/twitterbot
     ```
-2. Run the following command on your server to create a local SQLite database from the raw data:
+2. Create a local SQLite database from the raw data with:
     ```
     python3 database.py
     ```
 3. Sign up for a [Twitter developer account](https://developer.x.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api) (the free plan is sufficient) and obtain the credentials from the developer portal.
-4. Create a `cred.env` file and add your Twitter API credentials in the following format:: 
+4. Create a `cred.env` file and add your Twitter API credentials in the following format:
     ```
     consumer_key:XXXXXXX
     consumer_secret:XXXXXXX
@@ -55,7 +55,7 @@ Twitterbot is designed to run permanently and therefore you should run it on a s
     ```
     Press `i` to manipulate the file, `Esc` when finished and `:wq` to save and close.
     The most important parameters are those for the schedule. Choose a `SCHEDULE_MODE` and adjust their corresponding parameters. Change the allowed Tweet length if you have Twitter blue.
-6. Start the bot with 
+6. Run 
     ```
     nohup python3 twitterbot.py &
     ```
@@ -72,7 +72,7 @@ Twitterbot is designed to run permanently and therefore you should run it on a s
 ## Notes
 1. The script `database.py` also provides some other useful database related functions.
 2. The computed posting times and all the other logs will be written in a `twitterbot.log` file.
-3. Since I am German, the time data are in a 24hr format. If you use the AM/PM format, you should change the format variables in the `config.py` file to output (and read) the correct format. You can use `'%I:%M %p'` as time format. However, the code should work for you as well, since the `datetime` modul handles this internally.
+3. Since I am German, the time data are in a 24hr format. If you use the AM/PM format, you should change the format variables in the `config.py` file to output (and readout) the correct format. You can use `'%I:%M %p'` as time format. I haven't tested this extensively, however, no problems should occur since the `datetime` module handles this internally.
 
 ## Support
 
--- scheduler.py
+++ scheduler.py
@@ -12,12 +12,12 @@ class Scheduler:
         """
         # object variables
         self.schedule = None
-        self.mode = None
         self.generate = None
-
-        self.logger = self._init_logger()
+        self.logger = None
+        self.mode = None
+        
+        self._init_logger()
         self._set_mode()
-
         self.logger.debug(f"Scheduler initialized in '{self.mode}' mode.")
 
     def _init_logger(self):
@@ -32,23 +32,23 @@ class Scheduler:
         filehandler.setFormatter(formatter)
         logger.addHandler(filehandler)
 
-        return logger
+        self.logger = logger
 
     def _set_mode(self):
         """
-        Sets up the mode and the generate() function wrt the config file.
+        Sets up the mode and the generate() function wrt the config.
         """
-        # choose func wrt mode
+        # choose method to generate posting times
         self.generation_funcs = {
             'fix' : self._generate_fix,
             'uniform' : self._generate_uniform
         }
         self.mode = config.SCHEDULE_MODE
-        self.generate = self.generation_funcs[config.SCHEDULE_MODE]
+        self.generate = self.generation_funcs[self.mode]
 
     def _generate_fix(self):
         """
-        Computes the posting times given the input data in the config file. Varies the input data if config file indicates so.
+        Computes the posting times given the input data in the config file. Varies the input data if the configs indicate so.
         If no posting time would occur after execution of this code at the same day, the posting will be computed for the following day.
         """
         now = datetime.now().astimezone()
@@ -61,7 +61,7 @@ class Scheduler:
             # create time object from given time string
             t = datetime.strptime(t, config.TIME_FORMAT)
 
-            # vary time if indicated and ensure it don´t exceed 00:00 or 23:59
+            # vary time if indicated and ensure it doesn´t exceed 00:00 or 23:59
             lowb = min(t.minute, rv) if t.hour==0 else rv
             uppb = min(59-t.minute, rv) if t.hour==23 else rv
 
@@ -72,43 +72,40 @@ class Scheduler:
         # catch unsorted input data
         posting_times.sort()
 
-        # decide whether to compute times for today or tomorrow
+        # if no posting time would remain for today, compute for tomorrow
         posting_day = now.date() + timedelta(days=1) * (posting_times[-1] < now.time())
         
-        # create datetime objects and remove passed posting times (may occur on first day)
+        # create datetime objects and remove passed posting times (may occur if posting_day ist today)
         schedule = [datetime.combine(posting_day, t).astimezone() for t in posting_times]
         schedule = [posting_time for posting_time in schedule if now < posting_time]
 
         self.logger.debug(f"Generated posting times for [{posting_day.strftime(config.DATE_FORMAT)}]: {" ".join( t.strftime(config.TIME_FORMAT) for t in schedule )}")
-        
         self.schedule = schedule
 
     def _generate_uniform(self):
         """
         Computes the posting times evenly distributed within the given time window. Only allows up to 1 posting per minute. 
-        If no posting time would occur after execution of this code at the same day, the posting will be computed for the following day.
+        If no posting time would occur after execution of this code at the same day, the schedule will be computed for the following day.
         """
         now = datetime.now().astimezone()
 
         # parameters
-        postings = config.UNIFORM_MODE['postings']
-
-        # start and end time
+        num_posts = config.UNIFORM_MODE['num_posts']
         start_time, end_time = config.UNIFORM_MODE['time_window']
         start_time, end_time = datetime.strptime(start_time, config.TIME_FORMAT), datetime.strptime(end_time, config.TIME_FORMAT)
         total_space = (end_time-start_time).total_seconds()
 
         # compute distribution
-        space_per_post =  total_space // (postings+1)
+        space_per_post =  total_space // (num_posts+1)
         if space_per_post < 60:
             space_per_post = 60
-            postings = int(total_space//space_per_post - 1)
-            self.logger.warning(f"Posting {config.UNIFORM_MODE['postings']} posts from {start_time.strftime(config.TIME_FORMAT)} to {end_time.strftime(config.TIME_FORMAT)} is too much. Posting {int(postings)} posts instead (1 per minute).")
+            num_posts = int(total_space//space_per_post - 1)
+            self.logger.warning(f"Posting {config.UNIFORM_MODE['postings']} posts from {start_time.strftime(config.TIME_FORMAT)} to {end_time.strftime(config.TIME_FORMAT)} is too much. Posting {int(num_posts)} posts instead (1 per minute).")
 
         # add posting times iteratively
         schedule = []
         curr_time = start_time + timedelta(seconds=space_per_post)
-        while len(schedule) < postings:
+        while len(schedule) < num_posts:
             schedule.append(curr_time)
             curr_time += timedelta(seconds=space_per_post)
 
@@ -120,14 +117,12 @@ class Scheduler:
             datetime.combine(posting_day, posting_time.time()).astimezone() for posting_time in schedule
         ]
 
-        self.logger.debug(f"Generated {postings} posting times for [{posting_day.strftime(config.DATE_FORMAT)}]: {" ".join( t.strftime(config.TIME_FORMAT+':%S') for t in schedule )}")
-
+        self.logger.debug(f"Generated {num_posts} posting times for [{posting_day.strftime(config.DATE_FORMAT)}]: {" ".join( t.strftime(config.TIME_FORMAT+':%S') for t in schedule )}")
         self.schedule = schedule
-
 
     def sleep(self):
         """
-        Gets the runtime to sleep until the next posting time.
+        Gets the app sleeping until the next posting time.
         """
         # compute sleeping time
         now = datetime.now().astimezone()
@@ -140,7 +135,7 @@ class Scheduler:
 
     def is_empty(self):
         """
-        Returns if the list of posting times is empty. Intended to be used as the condition the main loop.
+        Returns if the list of posting times is empty. Intended to be used as the condition of the main loop.
         """
         return len(self.schedule)==0
 
